3931dac97ed94c7b8a0179e5712d1748368b1da8
MLSaj <60438701+MLSaj@users.noreply.github.com>
2020-02-19 11:57:54 -0500

    Add files via upload
    https://leetcode.com/problems/n-ary-tree-preorder-traversal/

---
+++ n-aryPreOrder.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        traversal = []
+        stack = [root]
+        while stack:
+            cur = stack.pop()
+            traversal.append(cur.val)
+            stack.extend(reversed(cur.children))
+        return traversal
